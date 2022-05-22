58b4e2f7d8b659e046ca9747c5126330ef26543b
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-22 15:41:20 +0200

    Estructura de proyecto.

---
+++ mska-app-backend/src/test/java/com/http/mska/MskaAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.mska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MskaAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
