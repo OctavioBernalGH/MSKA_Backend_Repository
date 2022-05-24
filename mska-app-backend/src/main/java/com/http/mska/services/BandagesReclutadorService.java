255604f1ea2e64b124d71f0a2591dc8e18ec5102
Ixabel Justo <miju355@vidalibarraquer.net>
2022-05-24 12:25:45 +0200

    "Se definen los métodos CRUD de la capa Service en la interface correspondiente"

--- mska-app-backend/src/main/java/com/http/mska/services/BandagesReclutadorService.java
+++ mska-app-backend/src/main/java/com/http/mska/services/BandagesReclutadorService.java
@@ -1,5 +1,24 @@
 package com.http.mska.services;
 
-public class BandagesReclutadorService {
+import java.util.List;
+
+import com.http.mska.dto.BandagesReclutador;
+
+public interface BandagesReclutadorService {
+	
+	//Listar los bandages de reclutador
+	public List<BandagesReclutador> listarBandagesReclutador();
+	
+	//Crear bandage de reclutador
+    public BandagesReclutador crearBandageReclutador(BandagesReclutador bandagesReclutador);
+
+    //Modificar bandage de reclutador
+    public BandagesReclutador modificarBandageReclutador(BandagesReclutador bandagesReclutador);
+
+    //Eliminar bandage de reclutador
+    public void eliminarBandageReclutador(Long id);
+    
+    //Buscar bandage de reclutador
+    public BandagesReclutador buscarBandageReclutador(Long id);
 
 }
