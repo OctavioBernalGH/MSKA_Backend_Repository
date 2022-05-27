7c2cea755664da4def2170deaf329a765bdf0e1a
Ixabel Justo <miju355@vidalibarraquer.net>
2022-05-27 10:53:10 +0200

    Todos los conflictos resueltos

--- mska-app-backend/src/main/java/com/http/mska/services/IBandageReclutadorService.java
+++ mska-app-backend/src/main/java/com/http/mska/services/IBandageReclutadorService.java
@@ -2,20 +2,6 @@ package com.http.mska.services;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import com.http.mska.dto.BandagesReclutador;
-
-public interface IBandageReclutadorService {
-	
-	//Listar los bandages de reclutador
-	public List<BandagesReclutador> listarBandagesReclutador();
-	
-	//Crear bandage de reclutador
-    public BandagesReclutador crearBandageReclutador(BandagesReclutador bandagesReclutador);
-
-    //Modificar bandage de reclutador
-    public BandagesReclutador modificarBandageReclutador(BandagesReclutador bandagesReclutador);
-=======
 import com.http.mska.dto.BandageReclutador;
 
 public interface IBandageReclutadorService {
@@ -28,16 +14,11 @@ public interface IBandageReclutadorService {
 
     //Modificar bandage de reclutador
     public BandageReclutador modificarBandageReclutador(BandageReclutador bandageReclutador);
->>>>>>> 520e4fcc104418849e4d79e38a728a6902fe25a7
 
     //Eliminar bandage de reclutador
     public void eliminarBandageReclutador(Long id);
     
     //Buscar bandage de reclutador
-<<<<<<< HEAD
-    public BandagesReclutador buscarBandageReclutador(Long id);
-=======
     public BandageReclutador buscarBandageReclutador(Long id);
->>>>>>> 520e4fcc104418849e4d79e38a728a6902fe25a7
 
 }
--- mska-app-backend/src/main/java/com/http/mska/services/IBandageTecnicoService.java
+++ mska-app-backend/src/main/java/com/http/mska/services/IBandageTecnicoService.java
@@ -2,20 +2,6 @@ package com.http.mska.services;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import com.http.mska.dto.BandagesTecnico;
-
-public interface IBandageTecnicoService {
-		
-		//Listar los bandages de técnico
-		public List<BandagesTecnico> listarBandagesTecnico();
-		
-		//Crear bandage de técnico
-	    public BandagesTecnico crearBandageTecnico(BandagesTecnico bandagesTecnico);
-
-	    //Modificar bandage de técnico
-	    public BandagesTecnico modificarBandageTecnico(BandagesTecnico bandagesTecnico);
-=======
 import com.http.mska.dto.BandageTecnico;
 
 public interface IBandageTecnicoService {
@@ -28,16 +14,12 @@ public interface IBandageTecnicoService {
 
 	    //Modificar bandage de técnico
 	    public BandageTecnico modificarBandageTecnico(BandageTecnico bandageTecnico);
->>>>>>> 520e4fcc104418849e4d79e38a728a6902fe25a7
 
 	    //Eliminar bandage de técnico
 	    public void eliminarBandageTecnico(Long id);
 	    
 	    //Buscar bandage de técnico
-<<<<<<< HEAD
-	    public BandagesTecnico buscarBandagesTecnico(Long id);
-=======
+
 	    public BandageTecnico buscarBandageTecnico(Long id);
->>>>>>> 520e4fcc104418849e4d79e38a728a6902fe25a7
 
 	}
