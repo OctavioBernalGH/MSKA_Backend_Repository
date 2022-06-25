daa04d0d412d56b4875579e382fb353a99fde60d
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-06-25 21:13:34 +0200

    camnbios raros

--- src/main/java/com/http/mska/MskaAppBackendApplication.java
+++ src/main/java/com/http/mska/MskaAppBackendApplication.java
@@ -2,8 +2,9 @@ package com.http.mska;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class MskaAppBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/http/mska/security/WebSecurity.java
+++ src/main/java/com/http/mska/security/WebSecurity.java
@@ -42,17 +42,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		 * 1. Se desactiva el uso de cookies 2. Se activa la configuración CORS con los
 		 * valores por defecto 3. Se desactiva el filtro CSRF 4. Se indica que el login
 		 * no requiere autenticación 5. Se indica que el resto de URLs esten securizadas
-		 */
+		 
 		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
 				.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL, REGISTER_URL).permitAll()
 				.anyRequest().authenticated().and().addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+				*/
+		
+		httpSecurity.httpBasic().disable();
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+		// auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	@Bean
