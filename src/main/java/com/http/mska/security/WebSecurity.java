a9d4392fe457631790672b2516cf4e35aa6a1717
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-06-25 21:15:54 +0200

    vuelta atras

--- src/main/java/com/http/mska/security/WebSecurity.java
+++ src/main/java/com/http/mska/security/WebSecurity.java
@@ -41,15 +41,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		/*
 		 * 1. Se desactiva el uso de cookies 2. Se activa la configuración CORS con los
 		 * valores por defecto 3. Se desactiva el filtro CSRF 4. Se indica que el login
-		 * no requiere autenticación 5. Se indica que el resto de URLs esten securizadas
+		 * no requiere autenticación 5. Se indica que el resto de URLs esten securizadas*/
 		 
 		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
 				.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL, REGISTER_URL).permitAll()
 				.anyRequest().authenticated().and().addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
-				*/
-		
-		httpSecurity.httpBasic().disable();
+				
 	}
 
 	@Override
