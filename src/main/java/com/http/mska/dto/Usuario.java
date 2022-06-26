491e2a25fb4e15e84bb9d64f21dbd672bc230ea1
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-06-26 15:38:06 +0200

    cambios

--- src/main/java/com/http/mska/dto/Usuario.java
+++ src/main/java/com/http/mska/dto/Usuario.java
@@ -570,6 +570,18 @@ public class Usuario{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}	
 }
